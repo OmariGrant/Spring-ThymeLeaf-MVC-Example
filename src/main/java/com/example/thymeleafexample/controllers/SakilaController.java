2c3eaa0cb1b7ff30e256d04575d05586ead7fd0f
OGrant <OmariGrant@users.noreply.github.com>
2023-01-12 08:20:02 +0000

    added Homepage

--- src/main/java/com/example/thymeleafexample/controllers/SakilaController.java
+++ src/main/java/com/example/thymeleafexample/controllers/SakilaController.java
@@ -2,6 +2,7 @@ package com.example.thymeleafexample.controllers;
 
 import com.example.thymeleafexample.entities.Actor;
 import com.example.thymeleafexample.repositories.ActorRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,10 @@ public class SakilaController {
 
 
 
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
     @GetMapping("/sakila/cast/add/{id}")
     public String addCastmember(@PathVariable int id, @ModelAttribute("castList")List<Actor> castList){
         Actor actor=actorRepository.findById(id).get();
